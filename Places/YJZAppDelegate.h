f4b94c41d636b5ab767abd0adb62c562656d4e04
Yiwen Zhan <theyiwen@gmail.com>
2014-07-19 14:09:28 -0700

    Initial Commit

---
+++ Places/YJZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  YJZAppDelegate.h
+//  Places
+//
+//  Created by Yiwen Zhan on 7/19/14.
+//  Copyright (c) 2014 Yiwen Zhan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YJZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Places/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Places
+//
+//  Created by Yiwen Zhan on 7/19/14.
+//  Copyright (c) 2014 Yiwen Zhan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "YJZAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([YJZAppDelegate class]));
+    }
+}
